c7e53355e1d8a2c703686a4b8eeed35a01194a69
vinayakkoli <vinayakkoli@gmail.com>
2022-12-08 13:29:08 +0530

    variable assigment
    variable assigment

---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+var myFullName;
+myFullName="Vinayak Koli";
+console.log(myFullName);
+myFullName="Vinayak Somning Koli";
+console.log(myFullName);
+var myLove="I Love Only JS";
+console.log(myLove);
+var firstName="Vinayak";
+var lastName="Koli";
+var age="23";
+var adress="At Post Nandur Tal-Mangalwedha Dist-Solapur ";
+var pin_code="413319";
+var gender="Male"
+var is_married="No";
+console.log(firstName,lastName,age,adress,gender,is_married);
