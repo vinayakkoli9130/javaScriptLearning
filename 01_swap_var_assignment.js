b6a99b7270f5f5cc47b17d97fe6f93c51c9985b4
vinayakkoli <vinayakkoli@gmail.com>
2022-12-09 15:28:58 +0530

    Swap Or Exchange Value Assignment
    Swap Or Exchange Value Assignment

---
+++ 01_swap_var_assignment.js
@@ -0,0 +1,29 @@
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("================Assignment-1.1=================");
+console.log("======Before Swap=======");
+console.log(sweety,cutie);
+var temp = sweety;
+sweety = cutie;
+cutie=temp;
+console.log("======After Swap=======");
+console.log(sweety,cutie);
+console.log("================Assignment-1.2=================");
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("======Before Swap=======");
+console.log(num1,num2,num3);
+var temp=num1
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("======After Swap=======");
+console.log(num1,num2,num3);
+
+
+
+// var temp = myNumber;
+// myNumber=yourNumber;
+// yourNumber=temp;
+// console.log(myNumber,yourNumber);
