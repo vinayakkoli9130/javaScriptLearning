614a57ef7fb95e712f68dfe44a5207e6548d0745
vinayakkoli <vinayakkoli@gmail.com>
2022-12-11 16:55:55 +0530

    create file to check which typeof datatype in Variable
    create file to check which typeof datatype in Variable

---
+++ 01_typesOf.js
@@ -0,0 +1,22 @@
+var bankName="SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+console.log("--------------------------------------------------------------------------------------------");
+
+var marks='Marks 90%'; 
+console.log( marks);
+console.log(typeof marks);
+console.log("--------------------------------------------------------------------------------------------")
+ var isWorking=true;
+ console.log("true" );
+
+console.log(typeof isWorking );
+console.log("--------------------------------------------------------------------------------------------")
+var totlCount="Hundred and Seven";
+console.log( totlCount);
+console.log(typeof totlCount);
+console.log("--------------------------------------------------------------------------------------------")
+
+var  totalCount=107;
+console.log(totalCount);
+console.log(typeof totalCount);
