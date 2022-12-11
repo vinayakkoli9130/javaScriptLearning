3722e0ec1ae0f6ddb1238070483b4e507283676b
vinayakkoli <vinayakkoli@gmail.com>
2022-12-11 16:46:49 +0530

    Funtion Assignment
    Funtion Assignment

---
+++ 02_normalFunction_Assignment.js
@@ -0,0 +1,6 @@
+function  bankDetails(bankName,accountNumber,location,pinCode) {
+   console.log("Bank Name-:", bankName, "Account Number-:", accountNumber, "Location-:", location,"Pin Code-:", pinCode); 
+}
+bankDetails("CITI Bank",3456782345,"Pune",431202 );
+bankDetails("Axis Bank", 7856782345, "Mumbai", 441202 );
+bankDetails("HDFC Bank", 8956782345, "Pune", 631202 );
