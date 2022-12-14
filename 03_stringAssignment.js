b4bb3c9798c6772639a176474ab9637c020c3b64
vinayakkoli <vinayakkoli@gmail.com>
2022-12-14 17:24:34 +0530

    Operators With String
    Operators With String

--- 03_stringAssignment.js
+++ 03_stringAssignment.js
@@ -1,6 +1,6 @@
 function squareOfWordLength(val) {
-    console.log('1)');
-    console.log(`        Word-:  ${val}`);
+   
+    console.log(`      Word-:  ${val}`);
     console.log(`length of Word-:  ${val.length}`);
     console.log(`length of Word Squre-:  ${val.length * val.length}`);
     
@@ -10,9 +10,13 @@ squareOfWordLength("Google Chrome");
 squareOfWordLength("Developer Smart");
 console.log("=============================================================================");
 var string=function () {
-    var str="I Am Angular Developer";
+   
+    var sentence = "I Am Angular Developer";
+var word =  sentence.split(" ");
+
+console.log(`length of string-: ${sentence.length} Total Number Of Words In String-: ${word.length} \n String Length Divided By Total Number of Words In String-:${sentence.length / word.length}`);
+console.log(`String Length Multiple By Total Number Of Words In String-:${sentence.length * word.length}`);
     
-    console.log(`String length-: ${str.length} Total Number Of words in String-: ${str.slice(" ")}`);
-    console.log(`String length-: ${str.length} Total Number Of words in String-: ${str.slice(" ")}`);
 }
 string();
+
