e4bc527941eb29114d1c513a0c74514874d612e1
vinayakkoli <vinayakkoli@gmail.com>
2022-12-14 17:23:37 +0530

    using Arithmatic operators
    using Arithmatic operators in this file

---
+++ ArithmaticOperators.js
@@ -0,0 +1,13 @@
+function arithmaticOperation(val1,val2) {
+    console.log("Values-:",val1,val2);
+    console.log("Addition",val1+val2);
+    console.log("Values-:",val1,val2);
+    console.log("Subtraction",val1 - val2);
+    console.log("Values-:",val1,val2);
+    console.log("Multiplication",val1 * val2);
+    console.log("Values-:",val1,val2);
+    console.log("Division",val1 / val2);
+
+}
+arithmaticOperation(12,6);
+arithmaticOperation(3393993,23233232);
