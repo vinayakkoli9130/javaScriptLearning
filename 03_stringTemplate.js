a608c29d8537f047d86e8de3998322fb1dbe4f11
vinayakkoli <vinayakkoli@gmail.com>
2022-12-14 16:36:56 +0530

    string template

---
+++ 03_stringTemplate.js
@@ -0,0 +1,13 @@
+function stringTemplateAssig() {
+    console.log(`My Dream Company is "Cognizant"`);
+    console.log("=======================================================");
+    var myHobby1="PlayingCricket"
+    var myHobby2="ListingSongs"
+    var myHobby3="WatchingMovies"
+    console.log(`My Hobbies are -:${myHobby1} ${myHobby2} ${myHobby3}`);
+    console.log("=========================================================");
+    var concat=myHobby1 .concat ( myHobby2,   myHobby3)
+    console.log(concat);
+    console.log("=========================================================");
+}
+stringTemplateAssig();
