e5b8cbf1a2f7304f02f44fc288401c2c907fdf78
vinayakkoli <vinayakkoli@gmail.com>
2022-12-14 16:38:00 +0530

    assignment of strings with index methods
    assignment of strings with index methods

---
+++ 03_stringAssignment.js
@@ -0,0 +1,18 @@
+function squareOfWordLength(val) {
+    console.log('1)');
+    console.log(`        Word-:  ${val}`);
+    console.log(`length of Word-:  ${val.length}`);
+    console.log(`length of Word Squre-:  ${val.length * val.length}`);
+    
+}
+squareOfWordLength("JavaScript");
+squareOfWordLength("Google Chrome");
+squareOfWordLength("Developer Smart");
+console.log("=============================================================================");
+var string=function () {
+    var str="I Am Angular Developer";
+    
+    console.log(`String length-: ${str.length} Total Number Of words in String-: ${str.slice(" ")}`);
+    console.log(`String length-: ${str.length} Total Number Of words in String-: ${str.slice(" ")}`);
+}
+string();
---
+++ 03_stringTemplateAssign2.js
@@ -0,0 +1,19 @@
+var stringHandsOn=function () {
+    var string=`     Hey you are doing good, keep it up     `
+        console.log(`1)String-:${string}`);
+        console.log(`2)Total Length Of String- :${string.length}`);
+         console.log(`3)After Removing Extra Spaces Using trim()Method-:${string.trim()}`);
+         var word=string.trim();
+         var count=string.length - word.length;
+         console.log(`4)Total number Of Remove Spaces-:${count}`);
+         console.log(`5)First Character On String-: ${word.charAt(0)} Last Character On String-: ${word.charAt(33)} `);
+       var cdn=word.split(" ")
+         console.log(`6)Total Numbers Of Word in String-:${cdn.length}`);
+
+    console.log(`7)printing Index Word "good" Position-:${word.indexOf("good")}`);
+    console.log(`8)Printing Substring Starting From The Index 22-:${word.slice(22)}`);
+    console.log(`9)String End With Word "up"-:${word.endsWith("up")}`);
+    console.log(`10)String Started With Word "Hey"-:${word.startsWith("Hey")}`);
+
+}
+    stringHandsOn();
