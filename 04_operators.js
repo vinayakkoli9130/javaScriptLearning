62e6a560033f234eae98156303c0ad9018d692de
vinayakkoli <vinayakkoli@gmail.com>
2022-12-15 10:33:22 +0530

    Operators in JavaScript
    Operators in JavaScript

--- 04_operators.js
+++ 04_operators.js
@@ -1,72 +1,62 @@
 console.log(`============ Arithmetic Operators =================`);
+var num1=10;
+var num2=5;
 var addResult = num1 + num2;
+console.log(`num1=${num1}   num2=${num2}`);
 console.log(`Addition is ${addResult} `);
 
-console.log(`Subtraction is ${num1-num2}`);
-var mulResult  = num1 * num2;
-console.log(`Multiplication is ${mulResult}`);
+ console.log(`Subtraction is ${num1-num2}`);
+// var mulResult  = num1 * num2;
+console.log(`Multiplication is ${num1 * num2}`);
  
-var divResult = num1 / num2;
-console.log(`Division is ${divResult}`);
+// var divResult = num1 / num2;
+console.log(`Division is ${num1 / num2}`);
 
-var modulusResult = num1 % num2;
-console.log(`Modulus is ${modulusResult}`);
+// var modulusResult = num1 % num2;
+ console.log(`Modulus is ${num1 % num2}`);
 
-var modulusResult = num1 % 4;
-console.log(`Modulus is ${modulusResult}`);
-console.log(`============ Unary Operators =================`);
-var num3 = 10;
-var result = ++num3; // num3 = num3 + 1;
-console.log(`Increment operator  result is ${result}`);
+// var modulusResult = num1 % 4;
+ console.log(`Modulus is ${num1 % 4}`);
+ console.log(`============ Unary Operators =================`);
+ var num3 = 10;
+ var result = ++num3; // num3 = num3 + 1;
+ console.log(`Increment operator  result is ${result}`);
 
-var num4 = 5;
-var res = --num4; //num4 -1;
-console.log(`Decrement operator result is ${res}`);
-console.log(`============ Assignment Operators =================`);
-var modulusResult = num1 % 3;
-console.log(`Modulus is ${modulusResult}`);
+ var num4 = 5;
+ var res = --num4; //num4 -1;
+ console.log(`Decrement operator result is ${res}`);
+ console.log(`============ Assignment Operators =================`);
+ var modulusResult = num1 % 3;
+ console.log(`Modulus is ${modulusResult}`);
 
-var num1 = 10;
-var num2 = 5;
-num1 += num2; // num1 = num1 + num2
-              // num1 = 15;
-console.log(`Compound Addition ${num1}`);
+// var num1 = 10;
+// var num2 = 5;
+//  num1 += num2; // num1 = num1 + num2
+//               // num1 = 15;
+ console.log(`Compound Addition ${num1+=num2}`);
 
-var num1 = 10;
-var num2 = 5;
-console.log(`Compound Subtraction ${num1-=num2}`); // num1 = 15 - 5
-console.log(`============ Assignment Operators =================`);
-var modulusResult = num1 % 3;
-console.log(`Modulus is ${modulusResult}`);
+// var num1 = 10;
+// var num2 = 5;
+ console.log(`Compound Subtraction ${num1-=num2}`); // num1 = 15 - 5
 
-var num1 = 10;
-var num2 = 5;
-num1 += num2; // num1 = num1 + num2
-              // num1 = 15;
-console.log(`Compound Addition ${num1}`);
+ console.log(`============ Comparison Operators =================`);
+// var num1 = 10;
+// var num2 = 5;
+// var num3 = 10
+// var result = num1 > num2;
+ console.log(`> Operator - Greater than ${num1>num2}`)
+// console.log(`> Operator - Greater than ${num1>num3}`)
 
-var num1 = 10;
-var num2 = 5;
-console.log(`Compound Subtraction ${num1-=num2}`); // num1 = 15 - 5
+// var result = num1 < num2;
+ console.log(`< Operator - Less than ${num1<num2}`)
 
-console.log(`============ Comparison Operators =================`);
-var num1 = 10;
-var num2 = 5;
-var num3 = 10
-var result = num1 > num2;
-console.log(`> Operator - Greater than ${result}`)
-console.log(`> Operator - Greater than ${num1>num3}`)
+// var result = num1 >= num3;
+ console.log(`>= Operator - Greater than equal ==> ${num1>=num2}`)
 
-var result = num1 < num2;
-console.log(`< Operator - Less than ${result}`)
+// var result = num1 <= num3;
+ console.log(`<= Operator - Less than equal ==> ${num1<=num2}`);
 
-var result = num1 >= num3;
-console.log(`>= Operator - Greater than equal ==> ${result}`)
-
-var result = num1 <= num3;
-console.log(`<= Operator - Less than equal ==> ${result}`);
-
-console.log(`Check even or ODD: ${11%2}`);
+ console.log(`Check even or ODD: ${11%2}`);
 
 
 
