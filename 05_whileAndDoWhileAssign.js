eda652d598093aeae76fc2c0c7287ae921605e6c
vinayakkoli <vinayakkoli@gmail.com>
2022-12-22 18:19:40 +0530

    while and do while Assignment
    while and do while Assignment

--- 05_whileAndDoWhileAssign.js
+++ 05_whileAndDoWhileAssign.js
@@ -10,6 +10,10 @@ do {
     console.log(num);
     num--;
 } while (num>=40);
+
+
+
+
 console.log(`Printing Number Using Do While Loop`);
 var str=5;
 do {
@@ -28,3 +32,5 @@ do {
     console.log(reverse);
     reverse=reverse-10;
 } while (reverse>=10);
+
+console.log(`Printing Odd Number 0 To 15 Using Do While Loop`);
