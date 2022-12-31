a7df96213a29f50ea86ab3450cb2a74dd4bef971
vinayakkoli <vinayakkoli@gmail.com>
2022-12-31 21:15:44 +0530

    Assignment Of freeze And Merge object
    Assignment Of freeze And Merge objec

---
+++ 07_ObjectArrayFreezeMerge.js
@@ -0,0 +1,39 @@
+let personalDetails={
+    name:"Vinayak Koli",
+    age:23,
+    town:"Mangalwedha"
+}
+console.log(`1st Object`);
+console.log("Personal Detalils",personalDetails);
+console.log(`____________________________________________________________________________________________________________`);
+let collegeDetails={
+collegeName:"Sangola College",
+city:"Sangola",
+degree:"BCS",
+passOutYear:2020,
+}
+console.log(`2nd Object`);
+console.log("Collge Detalils",personalDetails);
+console.log(`===============================Merge Two Objects=======================================================`);
+personalDetails=collegeDetails
+console.log(personalDetails);
+console.log(`____________________________________________________________________________________________________________`);
+console.log(`Do Not Add New Value In Array Because Of We Are Using freeze() Method `);
+let array=["Arman","Ganesh","Salman","jay","Dipak"];
+console.log(array);
+console.log(`Traverse String Using "for of" Loop`);
+console.log(`Given Friends Name Is--->`);
+for (const iterator of array) {
+   console.log(`${iterator}`); 
+}
+Object.freeze(array)
+// array.push("Javed")
+// console.log(array);
+console.log(`____________________________________________________________________________________________________________`);
+console.log(`Given a String Company="Codmind Technology"`);
+let arrayString="Codmind Technology";
+function wordsReverser(arrayString){
+    return arrayString.split('').reverse().join('');
+  }
+  console.log(`\nReverse Only Technology Word Of Given String--->Codemind`,wordsReverser('Technology'));
+console.log(`____________________________________________________________________________________________________`);
