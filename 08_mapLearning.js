3863ece0793afcb67a39b1219fb459385f2b0e2f
vinayakkoli <vinayakkoli@gmail.com>
2023-01-05 11:36:15 +0530

    Learning New Concept
    Learning New Concept like session Storage And Local Storage

--- 08_mapLearning.js
+++ 08_mapLearning.js
@@ -1,23 +1,60 @@
-
-
-
-
+//Map-:map can add keys and values.it is override method.they can change value.they don't allow same key
+//set-:set is used to add element in Map
+//get-:get is used to  initilize key value.
+//delete is used to delete key on the Map.
+//
 const mapOfNumbers = new Map(); // Creating an Map
 mapOfNumbers.set(11, "Eleven"); // Adding element in key value pair
-mapOfNumbers.set(22, "Twenty two");
+mapOfNumbers.set(22, "Twenty Two");
 mapOfNumbers.set(2, "Two");
 mapOfNumbers.set(11, "Ten + One"); 
 mapOfNumbers.set(33, "Eleven");
 console.log(mapOfNumbers);
 
-const valueForKey22 =  mapOfNumbers.get(22);
+const valueForKey22 =  mapOfNumbers.get(22);//get can be used to initilize key value.
 console.log(valueForKey22);
 
 const valueForKey11 =  mapOfNumbers.get(11);
 console.log(valueForKey11);
 
 console.log(mapOfNumbers);
-console.log(mapOfNumbers.size);
+console.log(mapOfNumbers.size);//size is used to find Map size.
 
-mapOfNumbers.delete(11);
+mapOfNumbers.delete(11);//delete is used to delete key on the Map
 console.log(mapOfNumbers);
+console.log("======== To check whether given key is available or not============");
+const isKeyAvailable = mapOfNumbers.has(22);//check given key is available or not
+console.log(isKeyAvailable);
+console.log(`Is key 1111 available: ${mapOfNumbers.has(1111)}`);
+
+console.log("===== Traversing over map====");//for of loop using traversing map
+const keysOfMap = mapOfNumbers.keys();
+console.log(keysOfMap);
+for (const key of keysOfMap) {
+    console.log(key,  mapOfNumbers.get(key));
+}
+
+class Employee {
+    constructor(empId, empName, empAge, empCity) {
+      this.empId = empId;
+      this.empName = empName;
+      this.empAge = empAge;
+      this.empCity = empCity;
+    }
+  }
+  const empSup = new Employee(11, "Supriya", 23, "PUNE");
+  const empDhir = new Employee(22, "Dheeraj", 24, "MUMBAI");
+  const empSachin = new Employee(33, "Sachin", 22, "Surat");
+  const empGayatri = new Employee(44, "Gayatri", 21, "Bangluru");
+  const mapOfEmployees = new Map(); // key -> empId   value -> employee object
+  mapOfEmployees.set(11, empSup);
+  mapOfEmployees.set(22, empDhir);
+  mapOfEmployees.set(33, empSachin);
+  mapOfEmployees.set(44, empGayatri);
+
+ const keyOfMapEmployee = mapOfEmployees.keys();
+ console.log(keyOfMapEmployee);
+ for (const key of keyOfMapEmployee) {
+     const employee = mapOfEmployees.get(key);
+     console.log(employee.empName, employee.empCity);
+ }
--- 08_setLearning.js
+++ 08_setLearning.js
@@ -1,4 +1,6 @@
-// Set: Allow unique elements
+// Set: Allow unique elements//Set Does Not Duplicate Element
+//add()-:adding elements in set//size-:used find the length of set.//delete()-:delete element in the set.
+//has()-: used to check element available or not in the set.//clear()-:used to clear set.
 const setNumbers = new Set(); // Creating Set
 console.log("Adding elements in set");
 setNumbers.add(0); // Add element
@@ -10,9 +12,8 @@ setNumbers.add(3); // Adding duplicate element - 3
 setNumbers.add(5);
 setNumbers.add(6);
 console.log(setNumbers);
-
 console.log(`Total available elements in Set`);
-const totalElements = setNumbers.size;
+const totalElements = setNumbers.size;//used to find size of set
 console.log(`Total elements in set : ${totalElements}`);
 
 console.log(`After deletion`);
@@ -29,7 +30,7 @@ console.log(
 );
 
 console.log(`Clearing set using clear() `);
-// setNumbers.clear();
+// setNumbers.clear();//clear method is used to clear set
 console.log(setNumbers);
 
 console.log(`Type of set is : ${typeof setNumbers}`);
@@ -45,7 +46,7 @@ let arrayOfNumbers = [5, 6, 4, 6, 5, 5, 6];
 // const myArray = [...mySet];
 // console.log(myArray);
 
-const myArray = [...new Set(arrayOfNumbers)];
+const myArray = [...new Set(arrayOfNumbers)];//using spread 
 console.log(myArray);
 
 class Employee {
@@ -64,7 +65,7 @@ const empGayatri = new Employee(44, "Gayatri", 21, "Bangluru");
 // Collection - Collection of elements 
 // Elements of Datatypes - Primitive and Non primitive (Reference)
 const arrayOfEmployees = [ empSup, empDhir, empGayatri, empSachin];
-for (const employee of arrayOfEmployees) {
+for (const employee of arrayOfEmployees) {//employee information using for of loop
     console.log(`Employee details are: ${employee.empId}  ${employee.empName} ${employee.empCity}  `);
 }
 
@@ -73,3 +74,30 @@ setOfEmployees.add(empSup);
 setOfEmployees.add(empDhir);
 setOfEmployees.add(empSachin);
 setOfEmployees.add(empGayatri);
+console.log(`---------------------------------------------------------------------------------------------------------------`);
+let setOfElements= new Set();
+ console.log(setOfElements);
+setOfElements.add(1)
+setOfElements.add(2)
+setOfElements.add(24)
+setOfElements.add(32)
+setOfElements.add(29)
+setOfElements.add(44)
+setOfElements.add(7);
+console.log(setOfElements);
+console.log(setOfElements.size);
+let deleteElement=setOfElements.delete(29)//delete element in set
+console.log(deleteElement);
+console.log(setOfElements.has(2));//available or not in set
+setOfElements.clear()//used to clear all set
+console.log(setOfElements);
+console.log(`:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::`);
+console.log(`::::::::::Removing Dublicate Elements::::::::::::::::::::::::`);
+const array=[36,34,24,5,4,7,89,24,5,4,433,4,5,6]
+console.log(array);
+//  let arrayNumber=new Set(array)//Remove duplicate element in array using set.
+//  console.log(arrayNumber);
+// const spread=[...new Set(array)]//Removing duplicate element in array using spread.
+// console.log(spread);
+// const object=new Set(array)
+// console.log(object);
