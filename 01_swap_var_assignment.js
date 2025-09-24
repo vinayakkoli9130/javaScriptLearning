0fc4035f040eb0b4eb704e38c91e4389b2ebd8f4
Vinayak Koli <vinayakkoli9130@gmail.com>
2025-09-24 11:00:13 +0530

    Update 01_swap_var_assignment.js

--- 01_swap_var_assignment.js
+++ 01_swap_var_assignment.js
@@ -1,125 +1,133 @@
 
-// function doSomething() {
-//     console.log(this);
-//   }
+// // function doSomething() {
+// //     console.log(this);
+// //   }
      
-//   doSomething();
-console.log("-------------1)Swap Two Variable Number Without Using Third Variable-------------");
-let a=10
-let b=20
-console.log("Before Swapping Variable-:",a,b);
-a=a+b
+// //   doSomething();
+// console.log("-------------1)Swap Two Variable Number Without Using Third Variable-------------");
+// let a=10
+// let b=20
+// console.log("Before Swapping Variable-:",a,b);
+// a=a+b
 
-b=a-b
+// b=a-b
 
-a=a-b
+// a=a-b
 
-console.log("After Swapping Variable-:",a,b);
-console.log("-------------2)Swap Two Variable Number Without Using Third Variable-------------");
-let firstNumber = 1;
-let secondNumber = 2;
+// console.log("After Swapping Variable-:",a,b);
+// console.log("-------------2)Swap Two Variable Number Without Using Third Variable-------------");
+// let firstNumber = 1;
+// let secondNumber = 2;
 
-console.log("Before Swap-:", firstNumber, secondNumber);
-firstNumber = firstNumber + secondNumber;
-secondNumber = firstNumber - secondNumber;
-firstNumber = firstNumber - secondNumber;
-console.log("After Swap-:", firstNumber, secondNumber);
+// console.log("Before Swap-:", firstNumber, secondNumber);
+// firstNumber = firstNumber + secondNumber;
+// secondNumber = firstNumber - secondNumber;
+// firstNumber = firstNumber - secondNumber;
+// console.log("After Swap-:", firstNumber, secondNumber);
 
-console.log("--------------------------------------------------------------------------------------------------------");
-var sweety="Sweety";
-var cutie="Cutie";
-console.log("================Assignment-1.1=================");
-console.log("======Before Swap=======");
-console.log(sweety,cutie);
-var temp = sweety;
-sweety = cutie;
-cutie=temp;
-console.log("======After Swap=======");
-console.log(sweety,cutie);
+// console.log("--------------------------------------------------------------------------------------------------------");
+// var sweety="Sweety";
+// var cutie="Cutie";
+// console.log("================Assignment-1.1=================");
+// console.log("======Before Swap=======");
+// console.log(sweety,cutie);
+// var temp = sweety;
+// sweety = cutie;
+// cutie=temp;
+// console.log("======After Swap=======");
+// console.log(sweety,cutie);
 
-console.log("================Assignment-1.2=================");
-var num1=100;
-var num2=200;
-var num3=300;
-console.log("======Before Swap=======");
-console.log(num1,num2,num3);
-var temp=num1
-num1=num2;
-num2=num3;
-num3=temp;
-console.log("======After Swap=======");
-console.log(num1,num2,num3);
+// console.log("================Assignment-1.2=================");
+// var num1=100;
+// var num2=200;
+// var num3=300;
+// console.log("======Before Swap=======");
+// console.log(num1,num2,num3);
+// var temp=num1
+// num1=num2;
+// num2=num3;
+// num3=temp;
+// console.log("======After Swap=======");
+// console.log(num1,num2,num3);
 
-var val1=70
-var val2=60;
-var val3=50;
-var val4=40;
-console.log('Give Values-:',val1,val2,val3,val4);
-var temp=val1
-val1=val2
-val2=val3;
-val3=val4;
-val4=temp;
-console.log('After Swap-:',val1,val2,val3,val4);
-
-
-// var temp = myNumber;
-// myNumber=yourNumber;
-// yourNumber=temp;
-// console.log(myNumber,yourNumber);
+// var val1=70
+// var val2=60;
+// var val3=50;
+// var val4=40;
+// console.log('Give Values-:',val1,val2,val3,val4);
+// var temp=val1
+// val1=val2
+// val2=val3;
+// val3=val4;
+// val4=temp;
+// console.log('After Swap-:',val1,val2,val3,val4);
 
 
-var va11="Vinayak"
-var va12="Vaibhav"
-var va13="Jyoti"
-console.log("Before Swap-:",va11,va12,va13);
-let temperory=va11
-va11=va12;
-va12=va13;
-va13=temperory
-console.log("Before Swap-:",va11,va12,va13);
-console.log("================================================================================");
-var number1=1
-var number2=2
-var number3=3
-console.log("Before Swap-:",number1,number2,number3);
+// // var temp = myNumber;
+// // myNumber=yourNumber;
+// // yourNumber=temp;
+// // console.log(myNumber,yourNumber);
 
-var tempory=number1
-number1=number2
-number2=number3
-number3=tempory
-console.log("After Swap-:",number1,number2,number3);
 
-console.log("Swap In Function");
-function swappingss(val1,val2,val3){
-    console.log("Before Swapping-:",val1,val2,val3);
-    var temp=val1
-val1=val2
-val2=val3
-val3=temp
+// var va11="Vinayak"
+// var va12="Vaibhav"
+// var va13="Jyoti"
+// console.log("Before Swap-:",va11,va12,va13);
+// let temperory=va11
+// va11=va12;
+// va12=va13;
+// va13=temperory
+// console.log("Before Swap-:",va11,va12,va13);
+// console.log("================================================================================");
+// var number1=1
+// var number2=2
+// var number3=3
+// console.log("Before Swap-:",number1,number2,number3);
 
-console.log("After Number Swapping-:",val1,val2,val3);
-}
-swappingss(1,2,3)
-swappingss("Mahesh","Ganesh","Dipak")
+// var tempory=number1
+// number1=number2
+// number2=number3
+// number3=tempory
+// console.log("After Swap-:",number1,number2,number3);
 
-var x="Mahesh"
-var y="Vinayak"
-console.log("Before Swap String-:",x,y);
-var n=x
-x=y
-y=n
-console.log("After Swapping String-:",x,y);
-console.log("Create Function To Swapping Two Number Without Using Third Value");
-function swap(a,b) {
-    console.log("Before Swapp-:",a,b);
+// console.log("Swap In Function");
+// function swappingss(val1,val2,val3){
+//     console.log("Before Swapping-:",val1,val2,val3);
+//     var temp=val1
+// val1=val2
+// val2=val3
+// val3=temp
+
+// console.log("After Number Swapping-:",val1,val2,val3);
+// }
+// swappingss(1,2,3)
+// swappingss("Mahesh","Ganesh","Dipak")
+
+// var x="Mahesh"
+// var y="Vinayak"
+// console.log("Before Swap String-:",x,y);
+// var n=x
+// x=y
+// y=n
+// console.log("After Swapping String-:",x,y);
+// console.log("Create Function To Swapping Two Number Without Using Third Value");
+// function swap(a,b) {
+//     console.log("Before Swapp-:",a,b);
+//     a=a+b
+//     b=a-b
+//     a=a-b
+//     console.log("After Swapp-:",a,b);
+    
+// }
+// swap(2,3)
+// swap(4,6)
+
+
+function swap(a,b){
+    console.log("Before Swap",a,b)
     a=a+b
     b=a-b
     a=a-b
-    console.log("After Swapp-:",a,b);
-    
+    console.log("After Swap")
 }
-swap(2,3)
-swap(4,6)
-
-
+swap(10,20)
